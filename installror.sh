a4d651d4564ef94a8698f0d1a2afd6729328dfe7
ra559 <arobertadrian@gmail.com>
2022-06-11 09:56:19 -0400

    updated

--- installror.sh
+++ installror.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 echo "This script will install the following packages:"
 PACK=('ruby-full' 'sqlite3' 'sqlitebrowser' 'libsqlite3-dev' 'yarn' 'nodejs' 'npm' 'rails')
+installer(){
+ sudo apt install ${PACK[@]} -y
+ gem install sqlite3 webdrivers childprocess xpath
+}
 for pack in ${PACK[@]}
 do
 	echo -e "\t 📦 $pack"
 done
 read -p "continue? " ans
-[[ $ans == "y" ]] && sudo apt install ${PACK[@]} -y || echo "Installation canceled"
+[[ $ans == "y" ]] && installer || echo "Installation canceled"
+
 
 
