25c51e548579764152f6bc3e49ebbf5a76cd1a0d
ra559 <arobertadrian@gmail.com>
2022-06-11 22:22:45 -0400

    fixed ruby installation script

--- installror.sh
+++ installror.sh
@@ -2,8 +2,8 @@
 echo "This script will install the following packages:"
 PACK=('ruby-full' 'sqlite3' 'sqlitebrowser' 'libsqlite3-dev' 'yarn' 'nodejs' 'npm' 'rails')
 installer(){
- sudo apt install ${PACK[@]} -y
- gem install sqlite3 webdrivers childprocess xpath
+  sudo apt install ${PACK[@]} -y
+  gem install sqlite3 webdrivers childprocess xpath
 }
 for pack in ${PACK[@]}
 do
