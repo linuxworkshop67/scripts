6d815092354df492ea2e6104719e6db82e198022
ra559 <arobertadrian@gmail.com>
2022-08-13 21:13:47 -0400

    install rails 7

--- installror7.sh
+++ installror7.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
-cd $HOME
-
-sudo apt install curl && curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash - && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-
+# The first step is to install some dependencies for Ruby and Rails.
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt update && sudo apt install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn -y
+sudo apt update && sudo apt install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn -yarn -y
 
-
-git clone https://github.com/rbenv/rbenv.git  && git clone https://github.com/rbenv/ruby-build.git 
-
+# Next we're going to be installing Ruby using a version manager called Rbenv.
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
 
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
 
-source $HOME/.bashrc
+# To install Ruby and set the default version, we'll run the following commands:
+rbenv install 3.1.2 && rbenv global 3.1.2
 
- ~/.rbenv
- ~/.rbenv/plugins/ruby-build
+# The last step is to install Bundler
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
 
-rbenv install 3.1.2
-rbenv global 3.1.2
 gem install bundler
-gem install rails -v 7.0.2.4
-rbenv rehash
-rails -v
 
