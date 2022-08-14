6505872b67578578c6c87d43b4562ca9506dc234
ra559 <arobertadrian@gmail.com>
2022-08-13 20:53:59 -0400

    install rails 7

--- installror7.sh
+++ installror7.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd $HOME
+
 sudo apt install curl
 curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -7,15 +9,17 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo apt update
 sudo apt install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn -y
 
-cd $HOME
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+
+git clone https://github.com/rbenv/rbenv.git 
+git clone https://github.com/rbenv/ruby-build.git 
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source $HOME/.bashrc
-
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 source $HOME/.bashrc
+
+~/.rbenv
+~/.rbenv/plugins/ruby-build
+
 rbenv install 3.1.2
 rbenv global 3.1.2
 gem install bundler
