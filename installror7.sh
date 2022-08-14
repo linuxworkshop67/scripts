79d920d360e0179735e893833f85151c434eab10
ra559 <arobertadrian@gmail.com>
2022-08-13 21:43:23 -0400

    install rails 7

--- installror7.sh
+++ installror7.sh
@@ -5,18 +5,20 @@ curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt update && sudo apt install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn -yarn -y
+sudo apt update && 
+
+sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn -y
+
 
 # Next we're going to be installing Ruby using a version manager called Rbenv.
 cd
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
+
 
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
 
 # To install Ruby and set the default version, we'll run the following commands:
 rbenv install 3.1.2 && rbenv global 3.1.2
@@ -29,3 +31,6 @@ source ~/.bashrc
 
 gem install bundler
 
+gem install rails -v 7.0.2.4
+rbenv rehash
+rails -v
